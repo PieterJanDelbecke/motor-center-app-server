8b8a292e1c5e5c1cb2a5a7df7dc19ee4d4901cdc
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-08-03 08:26:14 +1000

    feat: create new customer in DB

--- server.js
+++ server.js
@@ -1,152 +1,157 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const {
-    GraphQLSchema,
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLNonNull,
-    GraphQLList
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList,
+} = require("graphql");
+const { sequelize, customer, customer_info } = require("./models");
 
+const useRestRouter = require("./routes/rest-api");
+const useGraphRoute = require("./routes/graphql");
 
-} = require('graphql')
-const { sequelize, customer, customer_info} = require('./models')
+const app = express();
+const port = 3200;
 
-const useRestRouter = require('./routes/rest-api')
-const useGraphRoute = require('./routes/graphql')
+app.use(express.json());
+app.use("/api", useRestRouter);
 
-const app = express()
-const port = 3100
-
-app.use(express.json())
-app.use("/api",useRestRouter)
-
-
-const users = [
-    {id: 1, uuid: "UUID1", email: 'test1@email.com', password: 'password1', firstName: 'Adam', lastName: 'Ant', phoneNumber: "0405666111"},
-    {id: 2, uuid: "UUID2", email: 'test2@email.com', password: 'password2', firstName: 'Bob', lastName: 'Builder', phoneNumber: "0405666222"},
-    {id: 3, uuid: "UUID3", email: 'test3@email.com', password: 'password3', firstName: 'Charlie', lastName: 'Chan', phoneNumber: "0405666333"},
-]
-
-
-const UserType = new GraphQLObjectType({
-    name: 'User',
-    description:"These are the login details of an user",
-    fields: () => ({
-        id: {type: GraphQLNonNull(GraphQLInt)},
-        uuid: {type: GraphQLNonNull(GraphQLString)},
-        email: { type: GraphQLNonNull(GraphQLString)},
-        password: {type: GraphQLNonNull(GraphQLString)},
-        firstName: { type: GraphQLNonNull(GraphQLString)},
-        lastName: {type: GraphQLNonNull(GraphQLString)},
-        phoneNumber: { type: GraphQLNonNull(GraphQLString)},
-    })
-})
+const CustomerType = new GraphQLObjectType({
+  name: "User",
+  description: "These are the login details of an user",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    uuid: { type: GraphQLNonNull(GraphQLString) },
+    email: { type: GraphQLNonNull(GraphQLString) },
+    password: { type: GraphQLNonNull(GraphQLString) },
+    firstName: { type: GraphQLNonNull(GraphQLString) },
+    lastName: { type: GraphQLNonNull(GraphQLString) },
+    phoneNumber: { type: GraphQLNonNull(GraphQLString) },
+  }),
+});
 
 const RootQueryType = new GraphQLObjectType({
-    name: 'query',
-    description: 'Root Query',
-    fields: () => ({
-        users: {
-            type: new GraphQLList(UserType),
-            description: 'List of All Users',
-            resolve:  async (parent, args) => {
-                try{
-                    const customers = await customer.findAll()
-                    const clients = []
-                    for (const client of customers){
-                        const response = {
-                            id: client.id,
-                            uuid: client.uuid,
-                            email: client.email,
-                            password: client.password,
-                            firstName: client.first_name,
-                            lastName: client.last_name,
-                            phoneNumber: client.phone_number,
-                        }
-                        clients.push(response)
-                    }
-                    return clients
-                } catch (error){
-                    console.error(error)
-                }
-            }
-        },
-        user: {
-            type: UserType,
-            description: 'Gives a single User',
-            args: {
-                id: { type : GraphQLInt }
-            },
-            // resolve: (parent, args) => users.find( user => user.id === args.id)
-            resolve: async (parent, args) => {
-                try {
-                    const customerFound = await customer.findOne({
-                        where: {
-                            id: args.id
-                        }
-                    })
-                    const response = {
-                        id: args.id,
-                        uuid: customerFound.dataValues.uuid,
-                        password: customerFound.dataValues.password,
-                        firstName: customerFound.dataValues.first_name,
-                        lastName: customerFound.dataValues.last_name,
-                        phoneNumber: customerFound.dataValues.phone_number,
-                    }
-                    console.log("+++response: ", response)
-                    return response
-                } catch (error) {
-                    console.error(error)
-                }
-            }
+  name: "query",
+  description: "Root Query",
+  fields: () => ({
+    users: {
+      type: new GraphQLList(CustomerType),
+      description: "List of All Users",
+      resolve: async (parent, args) => {
+        try {
+          const customers = await customer.findAll();
+          const clients = [];
+          for (const client of customers) {
+            const response = {
+              id: client.id,
+              uuid: client.uuid,
+              email: client.email,
+              password: client.password,
+              firstName: client.first_name,
+              lastName: client.last_name,
+              phoneNumber: client.phone_number,
+            };
+            clients.push(response);
+          }
+          return clients;
+        } catch (error) {
+          console.error(error);
         }
-    })
-})
+      },
+    },
+    user: {
+      type: CustomerType,
+      description: "Gives a single User",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: async (parent, args) => {
+        try {
+          const customerFound = await customer.findOne({
+            where: {
+              id: args.id,
+            },
+          });
+          const response = {
+            id: args.id,
+            uuid: customerFound.dataValues.uuid,
+            password: customerFound.dataValues.password,
+            firstName: customerFound.dataValues.first_name,
+            lastName: customerFound.dataValues.last_name,
+            phoneNumber: customerFound.dataValues.phone_number,
+          };
+          console.log("+++response: ", response);
+          return response;
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    },
+  }),
+});
 
 const RootMutationType = new GraphQLObjectType({
-    name: 'mutation',
-    description: 'Mutation Query',
-    fields: () => ({
-        addUser:{
-            type: UserType,
-            description: "Adding a user",
-            args:{
-                email: { type: GraphQLNonNull(GraphQLString)},
-                password: { type: GraphQLNonNull(GraphQLString)},
-                firstName: { type: GraphQLNonNull(GraphQLString)},
-                lastName: { type: GraphQLNonNull(GraphQLString)},
-                phoneNumber: { type: GraphQLNonNull(GraphQLString)},
-            },
-            resolve: (package, args) => {
-                const user = {
-                    id: users.length +1,
-                    email: args.email,
-                    password: args.password,
-                    firstName: args.firstName,
-                    lastName: args.lastName,
-                    phoneNumber: args.phoneNumber
-                }
-                users.push(user)
-                return user
-            }
+  name: "mutation",
+  description: "Mutation Query",
+  fields: () => ({
+    addCustomer: {
+      type: CustomerType,
+      description: "Adding a Customer",
+      args: {
+        email: { type: GraphQLNonNull(GraphQLString) },
+        password: { type: GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLNonNull(GraphQLString) },
+        lastName: { type: GraphQLNonNull(GraphQLString) },
+        phoneNumber: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: async (package, args) => {
+        try {
+          const customersCount = await customer.count();
+          const newCustomer = await customer.create({
+            id: customersCount + 1,
+            email: args.email,
+            password: args.password,
+            first_name: args.firstName,
+            last_name: args.lastName,
+            phone_number: args.phoneNumber,
+          });
+          const response = {
+            id: newCustomer.dataValues.id,
+            uuid: newCustomer.dataValues.uuid,
+            email: newCustomer.dataValues.email,
+            password: newCustomer.dataValues.password,
+            firstName: newCustomer.dataValues.first_name,
+            lastName: newCustomer.dataValues.last_name,
+            phoneNumber: newCustomer.dataValues.phone_number,
+          };
+          return response;
+        } catch (error) {
+          console.log(error);
         }
-    })
-})
+      },
+    },
+  }),
+});
 
 const schema = new GraphQLSchema({
-    query: RootQueryType,
-    mutation: RootMutationType,
-})
+  query: RootQueryType,
+  mutation: RootMutationType,
+});
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema: schema,
-    graphiql: true
-}))
+    graphiql: true,
+  })
+);
 
 app.listen(port, async () => {
-    console.log(`+++ server running on port ${port} +++`)
-    // await sequelize.sync({force: true})
-    await sequelize.authenticate()
-    console.log("+++ DB synced +++")
-})
+  console.log(`+++ server running on port ${port} +++`);
+  // await sequelize.sync({force: true})
+  await sequelize.authenticate();
+  console.log("+++ DB synced +++");
+});
