85bf384dbdbe82a4f6acee58e0c039e5d71d57ed
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-08-04 07:21:12 +1000

    commit #10 by Pieter

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const { sequelize, customer, customer_info } = require("./models");
 const { RootMutationType, RootQueryType } = require("./graphql/gqlCustomers");
 
 // const useRestRouter = require("./routes/rest-api");
-// const useGraphRoute = require("./routes/graphql");
 
 const app = express();
 const port = 3200;
